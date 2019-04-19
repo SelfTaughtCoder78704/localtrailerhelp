0b767640bbd1e1873b0fda015adc567da43c2cc7
Bobby Nicholson <bobbynicholson78704@gmail.com>
2019-04-19 13:54:40 -0500

    beginning routes and bootstrap

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'TrailerFinder | Home'})
+    res.render('home', {title: 'LTH | Home'})
 })
 
 module.exports = router
--- routes/trailer-routes.js
+++ routes/trailer-routes.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+
+let trailers = [
+    {kind: '16ft Dump Trailer', img: 'https://betterdumptrailers.com/wp-content/uploads/2018/02/cropped-DumpTrailer_Gallery4-2.png'},
+    {kind: 'Tow Trailer', img: 'https://www.uhaul.com/Locations/GetPhoto.ashx?id=675471&size=5'},
+    {kind: 'Flatbed Trailer', img: 'http://www.kaufmantrailers.com/wp-content/uploads/2013/04/standard-flatbed-trailer.png'}
+]
+//get all trailers
 router.get('/', (req, res) => {
-    let trailers = [
-        {kind: '16ft Dump Trailer', img: 'https://betterdumptrailers.com/wp-content/uploads/2018/02/cropped-DumpTrailer_Gallery4-2.png'},
-        {kind: 'Tow Trailer', img: 'https://www.uhaul.com/Locations/GetPhoto.ashx?id=675471&size=5'},
-        {kind: 'Flatbed Trailer', img: 'http://www.kaufmantrailers.com/wp-content/uploads/2013/04/standard-flatbed-trailer.png'}
-    ]
+    
     res.render('trailers', {trailers: trailers, title: 'All Trailers'})
+})
+// post new trailer
+router.post('/', (req, res) => {
+    let newTrailer = {
+        kind: req.body.kind,
+        img: req.body.img
+    }
+    trailers.push(newTrailer)
+    res.redirect('/trailers')
+})
+// trailer form to submit 
+router.get('/new', (req, res) => {
+    res.render('add-trailer', {title: 'LTH | Add Trailer'})
 })
 
 module.exports = router
